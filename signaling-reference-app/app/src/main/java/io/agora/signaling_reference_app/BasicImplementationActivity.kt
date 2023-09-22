bd24cd65b124ddffd3a030a86d70b3dba71093f0
Saud <65331551+saudsami@users.noreply.github.com>
2023-09-22 09:38:10 +0500

    basic layout

--- signaling-reference-app/app/src/main/java/io/agora/signaling_reference_app/BasicImplementationActivity.kt
+++ signaling-reference-app/app/src/main/java/io/agora/signaling_reference_app/BasicImplementationActivity.kt
@@ -3,10 +3,13 @@ package io.agora.signaling_reference_app
 //import io.agora.agora_manager.AgoraManager
 //import io.agora.agora_manager.AgoraManager.AgoraManagerListener
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+
 
 open class BasicImplementationActivity : AppCompatActivity() {
     //protected lateinit var agoraManager: AgoraManager
@@ -25,9 +28,7 @@ open class BasicImplementationActivity : AppCompatActivity() {
         setContentView(layoutResourceId)
 
         // Set up access to the UI elements
-        btnJoinLeave = findViewById(R.id.btnJoinLeave) // The join/leave button
-        mainFrame = findViewById(R.id.main_video_container) // The main video frame
-        containerLayout = findViewById(R.id.containerLayout) // The multi-video container layout
+        btnJoinLeave = findViewById(R.id.btn_join_leave) // The join/leave button
 
         // Create an instance of the AgoraManager class
         initializeAgoraManager()
@@ -68,6 +69,27 @@ open class BasicImplementationActivity : AppCompatActivity() {
         }
 
      */
+    }
+
+    open fun displayMessage(messageText: String?, isSentMessage: Boolean) {
+        // Create a new TextView
+        val messageTextView = TextView(this)
+        messageTextView.text = messageText
+        messageTextView.setPadding(10, 10, 10, 10)
+        val messageList = findViewById<LinearLayout>(R.id.messageList)
+        val params = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT
+        )
+        if (isSentMessage) {
+            params.gravity = Gravity.END
+            messageTextView.setBackgroundColor(Color.parseColor("#DCF8C6"))
+            params.setMargins(100, 25, 15, 5)
+        } else {
+            messageTextView.setBackgroundColor(Color.parseColor("white"))
+            params.setMargins(15, 25, 100, 5)
+        }
+        // Add the textview to the linearlayout
+        messageList.addView(messageTextView, params)
     }
 
     protected fun showMessage(message: String?) {
